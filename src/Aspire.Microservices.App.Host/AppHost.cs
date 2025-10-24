79512ac6cb12a678d4d967c39d33e8f309f40b21
mucalytic <mucalytic@hotmail.com>
2025-10-24 20:01:39 +0200

    Configured app host.

--- src/Aspire.Microservices.App.Host/AppHost.cs
+++ src/Aspire.Microservices.App.Host/AppHost.cs
@@ -1,6 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Aspire_Microservices_Api_Notes>("notes-api");
-builder.AddProject<Projects.Aspire_Microservices_Api_Tags>("tags-api");
-
+var postgres = builder
+       .AddPostgres("postgres")
+       .WithHostPort(5432)
+       .WithDataVolume()
+       .WithLifetime(ContainerLifetime.Persistent);
+var notesDb = postgres.AddDatabase("notes"); 
+var tagsDb = postgres.AddDatabase("tags"); 
+var tagsApi = builder
+       .AddProject<Projects.Aspire_Microservices_Api_Tags>("tags-api")
+       .WithReference(tagsDb)
+       .WaitFor(tagsDb);
+builder.AddProject<Projects.Aspire_Microservices_Api_Notes>("notes-api")
+       .WithHttpsEndpoint(5001, name: "public")
+       .WithReference(notesDb)
+       .WithReference(tagsApi)
+       .WaitFor(notesDb)
+       .WaitFor(tagsApi);
 builder.Build().Run();
