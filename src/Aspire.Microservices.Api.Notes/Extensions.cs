2adaaa92e918f98980e3b55fbe72c2a34a1ffb74
mucalytic <mucalytic@hotmail.com>
2025-11-03 11:02:20 +0100

    Upgraded projects to .NET 10. Moved extensions to use extension member syntax.

--- src/Aspire.Microservices.Api.Notes/Extensions.cs
+++ src/Aspire.Microservices.Api.Notes/Extensions.cs
@@ -4,11 +4,14 @@ namespace Aspire.Microservices.Api.Notes;
 
 public static class Extensions
 {
-    public static WebApplication MigrateDatabase(this WebApplication app)
+    extension(WebApplication app)
     {
-        using var scope = app.Services.CreateScope();
-        using var context =  scope.ServiceProvider.GetRequiredService<NotesContext>();
-        context.Database.Migrate();
-        return app;
+        public WebApplication MigrateDatabase()
+        {
+            using var scope = app.Services.CreateScope();
+            using var context =  scope.ServiceProvider.GetRequiredService<NotesContext>();
+            context.Database.Migrate();
+            return app;
+        }
     }
 }
